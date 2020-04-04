fa49d17a56062880fe67c8f24eaa1adab180d9d8
Dalton Luis Boer <dluisbr@yahoo.com.br>
2020-04-04 13:41:23 -0300

    Curso SpringBoot Java 11

---
+++ src/test/java/com/me/dalton/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.dalton.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
