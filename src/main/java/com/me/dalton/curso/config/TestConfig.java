06512686ea6739b26c9740aca647ae6e1a73ba7e
Dalton Luis Boer <dluisbr@yahoo.com.br>
2020-04-05 19:34:12 -0300

    Product

--- src/main/java/com/me/dalton/curso/config/TestConfig.java
+++ src/main/java/com/me/dalton/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.me.dalton.curso.entities.Category;
 import com.me.dalton.curso.entities.Order;
+import com.me.dalton.curso.entities.Product;
 import com.me.dalton.curso.entities.User;
 import com.me.dalton.curso.entities.enums.OrderStatus;
 import com.me.dalton.curso.repositories.CategoryRepository;
 import com.me.dalton.curso.repositories.OrderRepository;
+import com.me.dalton.curso.repositories.ProductRepository;
 import com.me.dalton.curso.repositories.UserRepository;
 
 @Configuration
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -44,11 +49,21 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1,o2, o3));
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		
 	}
 	
